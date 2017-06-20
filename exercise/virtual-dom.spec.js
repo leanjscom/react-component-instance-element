c4014b870dc6125811bc134d6930613ec80e7db4
Alex Lobera <alex.lobera@gmail.com>
2017-06-20 17:43:41 +0100

    Update virtual-dom.spec.js

--- exercise/virtual-dom.spec.js
+++ exercise/virtual-dom.spec.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-dom/test-utils'
 import { expect } from 'chai'
-import '../jsdom-setup.js'
+import '../jsdom-setup'
 import TodoList,{ ListItem } from '../src/TodoList'
 
 describe('React Compoment, React Instance and React Element:', () => {
